e9a4a637b540db8ad0f7ebbd3266af465bb2bdec
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-10-24 10:48:21 -0700

    feat: update to 3.0.0

---
+++ script/bin/vkey.rs
@@ -0,0 +1,16 @@
+//! To build the binary:
+//!
+//!     `cargo build --release --bin genesis`
+//!
+use sp1_sdk::{HashableKey, ProverClient};
+use sp1_vectorx_script::SP1_VECTOR_ELF;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = ProverClient::new();
+    let (_pk, vk) = client.setup(SP1_VECTOR_ELF);
+
+    println!("VKEY={}", vk.bytes32());
+
+    Ok(())
+}
