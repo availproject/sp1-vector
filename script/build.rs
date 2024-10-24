0f904a9bb0401a5a4ff6a841f9fba86ebd2b0f08
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-10-24 12:33:04 -0700

    sp1 3.0.0

--- script/build.rs
+++ script/build.rs
@@ -2,12 +2,12 @@
 use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
-    build_program_with_args(
-        "../program",
-        BuildArgs {
-            docker: true,
-            elf_name: "sp1-vector-docker".to_string(),
-            ..Default::default()
-        },
-    );
+    // build_program_with_args(
+    //     "../program",
+    //     BuildArgs {
+    //         docker: true,
+    //         elf_name: "sp1-vector-docker".to_string(),
+    //         ..Default::default()
+    //     },
+    // );
 }
