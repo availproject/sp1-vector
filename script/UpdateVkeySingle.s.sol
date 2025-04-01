251cef550be7eda3085438422f936214d01e2a17
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-03-31 18:17:31 -0700

    feat: all changes

---
+++ script/UpdateVkeySingle.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Vm.sol";
+
+contract UpdateVkeySingleScript {
+    function run() public {
+        // Your script logic here
+    }
+}
